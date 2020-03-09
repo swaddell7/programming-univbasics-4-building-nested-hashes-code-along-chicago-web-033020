0154f8b463af2e26f10b6503ddbfd52005c3dad5
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-09 19:20:30 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,9 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {:name => "Benvolio", :age => "17", :attitude => "worried"
+        ]
    }, 
    :capulet => {
       :patriarch => {
